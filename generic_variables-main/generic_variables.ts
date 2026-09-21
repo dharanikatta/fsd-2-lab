519530a710e70812522767d259330526b6d2e7b6
dharanikatta <dharanikatta007@gmail.com>
2026-09-21 10:02:14 +0530

    Delete generic_variables-main directory

--- generic_variables-main/generic_variables.ts
+++
@@ -1,9 +0,0 @@
-function display<T>(value: T): void {
-    console.log("Value: " + value);
-}
-
-let num: number = 100;
-let name: string = "Adithi";
-
-display<number>(num);
-display<string>(name);
